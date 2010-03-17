e3890489954942312ea259ab31ad7910feff4c67
Maurits van Rees <maurits@vanrees.org>
2010-03-17 18:14:00 +0000

    no setup.cfg, no setup_requires or paster_plugins.
    svn path=/collective.setdefaulteditor/trunk/; revision=113338

--- setup.py
+++ setup.py
@@ -32,6 +32,4 @@ setup(name='collective.setdefaulteditor',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
