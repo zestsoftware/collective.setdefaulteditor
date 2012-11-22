a98cbfc9ba0e1c2df3c1a79dfcb998cfb8ea58ec
Maurits van Rees <maurits@vanrees.org>
2012-11-22 20:54:57 +0100

    Move logic from the template to the browser view.
    And make the browser code a bit more logic.

--- collective/setdefaulteditor/browser.py
+++ collective/setdefaulteditor/browser.py
@@ -3,10 +3,13 @@ from Acquisition import aq_inner
 from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
 from Products.statusmessages.interfaces import IStatusMessage
+from zope.i18nmessageid import MessageFactory
 
 from collective.setdefaulteditor.utils import set_editor_for_all
 
 logger = logging.getLogger('collective.setdefaulteditor')
+#_ = MessageFactory('collective.setdefaulteditor')
+PMF = MessageFactory('plone')
 
 
 class SetEditor(BrowserView):
@@ -22,38 +25,69 @@ class SetEditor(BrowserView):
         marker = object()
         context = aq_inner(self.context)
         pprops = getToolByName(context, 'portal_properties')
-        site_props = pprops.site_properties
-        self.available_editors = list(site_props.available_editors)
-        site_default_editor = site_props.getProperty('default_editor', marker)
-        # Add empty string for using site default.
-        if site_default_editor is not marker:
-            self.available_editors.append('')
         md = getToolByName(context, 'portal_memberdata')
-        self.default_editor = md.getProperty('wysiwyg_editor', 'None')
-
         wanted_editor = self.request.get('editor', marker)
+        site_props = pprops.site_properties
+        site_default_editor = site_props.getProperty('default_editor', marker)
+        member_default_editor = md.getProperty('wysiwyg_editor', marker)
+
+        # Determine the default editor.
+        if member_default_editor is marker:
+            if site_default_editor is marker:
+                self.default_editor = 'None'
+            else:
+                self.default_editor = site_default_editor
+        else:
+            self.default_editor = member_default_editor
+
+        # Get possible editors.
+        editors = list(site_props.getProperty('available_editors', []))
+        if '' not in editors:
+            # Add empty string for using site default.
+            editors.append('')
+        self.available_editors = []
+        for editor in editors:
+            if editor == 'None':
+                title = PMF(u"label_ordinary_content_editor",
+                            default=(u"Basic HTML textarea editor (works in "
+                                     u"all browsers)"))
+            elif editor == '':
+                title = PMF(u"vocabulary-available-editor-novalue",
+                            default=u"Use site default")
+            else:
+                title = editor
+            if editor == wanted_editor:
+                selected = 'selected'
+            else:
+                selected = None
+            self.available_editors.append(dict(
+                value=editor,
+                selected=selected,
+                title=title))
+
+        # Only POST requests allowed beyond this point.
+        if self.request.get('REQUEST_METHOD') != 'POST':
+            return
+
+        # Check the wanted editor.
         if wanted_editor is marker:
             # nothing to do
             return
-
         status = IStatusMessage(self.request)
-        if wanted_editor not in self.available_editors:
+        if wanted_editor not in editors:
             msg = "%r is not available as editor. Choose one of %r." % (
-                wanted_editor, self.available_editors)
+                wanted_editor, editors)
             status.addStatusMessage(msg, type='error')
             return
 
+        # Change the editor for all members.
         dry_run = (self.request.get('dryrun', 'off') == 'on')
         same, changed = set_editor_for_all(wanted_editor, dry_run=dry_run)
         msg = "Done. %d were the same; %d needed changing." % (same, changed)
         status.addStatusMessage(msg, type='info')
         logger.info(msg)
-        if dry_run:
-            msg = 'Dry-run selected: nothing changed.'
-            status.addStatusMessage(msg, type='warning')
-            logger.info(msg)
-            return
 
+        # Update the default editor.
         if self.request.get('update-default'):
             if wanted_editor == '':
                 msg = "Cannot set wysiwyg_editor for new members to nothing."
@@ -62,13 +96,22 @@ class SetEditor(BrowserView):
             else:
                 if site_default_editor is marker:
                     # Plone 3, usually.
-                    md._updateProperty('wysiwyg_editor', wanted_editor)
+                    if not dry_run:
+                        md._updateProperty('wysiwyg_editor', wanted_editor)
                     msg = "Updated wysiwyg_editor for new members to %r" % (
                         wanted_editor)
                 else:
-                    site_props._updateProperty('default_editor', wanted_editor)
-                    md._updateProperty('wysiwyg_editor', '')
-                    msg = "Updated default wysiwyg_editor to %r. New members will use the site default." % (
-                        wanted_editor)
+                    if not dry_run:
+                        site_props._updateProperty(
+                            'default_editor', wanted_editor)
+                        md._updateProperty('wysiwyg_editor', '')
+                    msg = ("Updated default wysiwyg_editor to %r. New members "
+                           "will use the site default." % (wanted_editor))
                 logger.info(msg)
                 status.addStatusMessage(msg, type='info')
+
+        if dry_run:
+            msg = 'Dry-run selected: nothing changed.'
+            status.addStatusMessage(msg, type='warning')
+            logger.info(msg)
+            return
